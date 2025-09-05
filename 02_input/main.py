187de461df41fc368d381bba0e04a3845a8b82f3
GoncaloaaF <goncaloaaf@gmail.com>
2025-09-05 13:40:46 +0100

    demo input

---
+++ 02_input/main.py
@@ -0,0 +1,7 @@
+nome = input("Digite seu nome: ")
+idade = int(input("Digite a sua idade: ")) # se o val numerico for usar para calc converter para int ou flaot
+
+ano_nasc = 2025 - idade
+
+print(f"O {nome} nasceu em {ano_nasc}.")
+
