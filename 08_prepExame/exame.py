59260f0cb6805af56ead2d9f4584533099095a18
GoncaloaaF <goncaloaaf@gmail.com>
2025-09-24 12:21:41 +0100

    prep exame

---
+++ 08_prepExame/exame.py
@@ -0,0 +1,139 @@
+"""
+Nome:
+Numero:
+Turma:
+
+"""
+
+#
+# ------
+# peça ao utilizado - input()
+# mostre na consola - print()
+# ------
+# a func recebe - param da func
+# devolve - return
+#
+
+"""
+
+Enunciado: faça uma função que some dois valores,
+
+nome da func: ex1
+numero: de param 2 
+ordem do para: irrelevante
+retrun: valor da soma
+
+"""
+"""
+def ex1(val1, val2):
+    soma = val1 + val2 # soma os 2 val e atribui a var soma
+    return soma        # devolve a var soma
+
+
+def ex1_errado(val1, val2):
+    soma = val1 + val2
+    print(soma)
+"""
+"""
+soma 4 num 2 a 2
+
+"""
+def exX():
+    n = ex1(12,12)
+    n2 = ex1(2, 13)
+    soma = n + n2
+    return soma
+
+
+
+"""
+soma 4 num 2 a 2
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+Cria uma função que recebe uma string e devolve a string invertida.
+
+nome da funcao: ex1
+numero: de param 1
+parametros: my_str
+out: my_str invertida
+
+"""
+
+def ex1(my_str):
+    """
+    inverte a str pq quando uso o slice ::-1 estou a percorrer a lista
+    das pos 0 até a última de tras para a frente, devido ao step ser -1
+    """
+    return my_str[::-1]
+
+
+"""
+ex1 ≠ Ex1 ≠ ex_1
+
+"""
+
+
+"""
+peça ao utilizado - input()
+mostre na consola - print()
+
+devolve - return
+a func recebe - param da func
+
+crie uma func que peça ao utilizado um num e mostre na consola se o numero e par ou impar 
+
+
+"""
+
+def ex6():
+    numero = int(input("Digite um numero: "))
+    if numero % 2 == 0:
+        print("par")
+    else:
+        print("impar")
+
+
+
+
+"""
+Contar Vogais
+Cria uma função que recebe uma frase e conta o número de vogais (a, e, i, o, u).
+
+
+nome da funcao: ex3
+numero: de param 1
+parametros: my_str
+out: 1 param com o numero de vogais 
+
+"""
+"""
+Cria uma função que recebe uma lista de números e devolve duas listas:
+- Uma com os pares
+- Outra com os ímpares
+
+
+nome da funcao: ex4
+numero: de param 1
+parametros: lst
+out: parm com as duas listas, 1º par, 2º impar 
+
+"""
+
+
+
+
---
+++ 08_prepExame/main.py
@@ -0,0 +1,14 @@
+from exame import *
+
+
+
+
+print("AS as as".isalpha())
+
+print("AS as as".isalnum())
+
+print("       a â  ")
+print("  a â  ".strip())
+
+print("AS".isalpha())
+print("AS12".isalnum())
